89814c38dac36915495e4cc668d8eb9c9a2accc3
subramanivasu <subramaniyanvasu@gmail.com>
2025-08-21 23:02:11 +0530

    Created patient-service with gePatients functionality

---
+++ patient-service/src/main/java/com/pm/patientservice/mapper/PatientMapper.java
@@ -0,0 +1,18 @@
+package com.pm.patientservice.mapper;
+
+import com.pm.patientservice.dto.PatientResponseDTO;
+import com.pm.patientservice.model.Patient;
+
+public class PatientMapper {
+
+    public static PatientResponseDTO toDTO(Patient patient){
+        PatientResponseDTO patientResponseDTO = new PatientResponseDTO();
+        patientResponseDTO.setId(String.valueOf(patient.getId()));
+        patientResponseDTO.setName(patient.getName());
+        patientResponseDTO.setAddress(patient.getAddress());
+        patientResponseDTO.setEmail(patient.getEmail());
+        patientResponseDTO.setDateOfBirth(patient.getBirthDate().toString());
+
+        return patientResponseDTO;
+    }
+}
